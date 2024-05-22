eddfc2583e6719b960b518ad44e1012c31b2de60
Abhishek Raj <118152707+Abhishekbotx@users.noreply.github.com>
2024-05-23 03:17:34 +0530

    test

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const path = require('path');
 // const cloudinary=require('./src/config/cloudinaryConfig')
 
 
-const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:3002/",
-    // "http://localhost:5175/",
-    "https://finmapxfrontendx.vercel.app/",
-  ];
+// const allowedOrigins = [
+//     "http://localhost:3000",
+//     "http://localhost:3002/",
+//     // "http://localhost:5175/",
+//     "https://finmapxfrontendx.vercel.app/",
+//   ];
   
   app.use(
     cors({
-      origin: allowedOrigins,
+      origin: "https://finmapxfrontendx.vercel.app",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
