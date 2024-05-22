4dd3d85fd67384b965e5fef004c9f8328e5f6b5f
Abhishek Raj <118152707+Abhishekbotx@users.noreply.github.com>
2024-05-23 02:15:12 +0530

    test

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const path = require('path');
 // const cloudinary=require('./src/config/cloudinaryConfig')
 
 
-
-
 const allowedOrigins = [
     "http://localhost:3001/",
     "http://localhost:3002/",
@@ -43,46 +41,9 @@ const allowedOrigins = [
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.use(cors(
-    {
-        origin:'http://localhost:3000',
-        credentials:true
-    }
-));
- 
-
-
-
 app.use( express.static(path.join('./src/utils/uploads')));
 
-// Endpoint to list all images in 'public/uploads/reviewImages'
-// app.get('/api/images', (req, res) => {
-//     const imagesDir = path.join(__dirname, 'public/uploads/reviewImages');
-//     fs.readdir(imagesDir, (err, files) => {
-//         if (err) {
-//             return res.status(500).send('Unable to scan directory');
-//         }
-//         const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif)$/.test(file));
-//         const imageUrls = imageFiles.map(file => `${req.protocol}://${req.get('host')}/public/uploads/reviewImages/${file}`);
-//         res.json(imageUrls);
-//     }); 
-// });
+
 
 app.use(express.static('/public'))
 const apiRoutes = require('./src/routes/index');
