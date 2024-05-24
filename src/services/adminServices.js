acccbe9ece3ce51abf0b89a448462c415a035d7b
Abhishek Raj <118152707+Abhishekbotx@users.noreply.github.com>
2024-05-24 16:16:48 +0530

    "check"

--- src/services/adminServices.js
+++ src/services/adminServices.js
@@ -242,10 +242,10 @@ class AdminService {
             throw error;
         }
     }
-    async deleteReview(data) {
+    async deleteReview(reviewId) {
         try {
 
-            const Review = await adminRepository.deleteReview(data.reviewId);
+            const Review = await adminRepository.deleteReview(reviewId);
             return Review
 
         } catch (error) {
