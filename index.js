28496e2b938b921095be30d9507639ed60304188
Abhishek Raj <118152707+Abhishekbotx@users.noreply.github.com>
2024-05-28 00:27:39 +0530

    context for routes

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const path = require('path');
   
   app.use(
     cors({
-      origin: "https://finmapxfront.vercel.app",
+      // origin: "https://finmapxfront.vercel.app",
+      origin: "http://localhost:3000",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
