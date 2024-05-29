ecf871b78d32ded5364f9e18770adaa888f0a3c9
Abhishek Raj <118152707+Abhishekbotx@users.noreply.github.com>
2024-05-29 20:33:18 +0530

    'minor slash'

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const path = require('path');
   
   app.use(
     cors({
-      origin: "https://finmapxfront.vercel.app",
+      origin: "https://finmapxfront.vercel.app/",
       // origin: "http://localhost:3000",
       credentials: true, 
       methods: ["GET", "POST", "PUT", "DELETE"],
